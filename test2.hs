07c3a761bb5f6d88f60331886d48abcfea004850
Aidan K. Ewart <aidanprattewart@gmail.com>
2020-04-09 14:21:15 +0000

    sourcepos to test2

--- test2.hs
+++ test2.hs
@@ -8,6 +8,9 @@ nextPos (Pos a b) = Pos a (b+1)
 
 data Stream = Stream String SourcePos
 
+getPos :: Stream -> SourcePos
+getPos (Stream _ p) = p
+
 type Parser a = Stream -> Maybe (a, Stream)
 
 many1 :: Parser a -> Parser [a]
@@ -15,6 +18,9 @@ many1 fn str = case fn str of
     Just (l, rem) -> case many1 fn rem of
         Just (ls, rem') -> Just (l:ls, rem')
         Nothing -> Just (l:[], rem)
+
+recordPos :: Parser a -> Parser (SourcePos, a)
+recordPos fn str = let pos = getPos str in fmap (\a -> (pos, a)) (fn str)
 
 satisfies :: (Char -> Bool) -> Parser Char
 satisfies condition (Stream (c:str) pos) = if condition c then Just (c, (Stream str (nextPos pos))) else Nothing
