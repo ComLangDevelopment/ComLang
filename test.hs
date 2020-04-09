b3d77166d4c8e6b4dde8fbb4f5957f1a9e40931d
Ray Redondo <rdrpenguin04@gmail.com>
2020-04-09 08:54:56 -0500

    Add types to tokens

--- test.hs
+++ test.hs
@@ -1,5 +1,12 @@
 import System.IO
 
+data TokenKind
+  = Identifier
+  | Comment
+  | ForwardSlash
+  | Other
+  deriving (Show)
+
 tokenizeIdentifier :: String -> [String]
 tokenizeIdentifier [] = []
 tokenizeIdentifier (a : b) = do
@@ -7,20 +14,20 @@ tokenizeIdentifier (a : b) = do
     x | ('0' <= x && x <= '9') || ('A' <= x && x <= 'Z') || ('a' <= x && x <= 'z') || (x == '_') -> (a : (tokenizeIdentifier b) !! 0) : tail (tokenizeIdentifier b)
     _                                                                                            -> "" : [a:b]
 
-tokenize :: (String, Integer) -> [(String, Integer)]
+tokenize :: (String, Integer) -> [(String, Integer, TokenKind)]
 tokenize ([],_) = []
 tokenize (a : b, c) = do
   case a of
-    x | ('A' <= x && x <= 'Z') || ('a' <= x && x <= 'z') || (x == '_') -> (tokenizeIdentifier (a : b) !! 0, c) : (tokenize (tokenizeIdentifier (a : b) !! 1, c))
+    x | ('A' <= x && x <= 'Z') || ('a' <= x && x <= 'z') || (x == '_') -> (tokenizeIdentifier (a : b) !! 0, c, Identifier) : (tokenize (tokenizeIdentifier (a : b) !! 1, c))
     '\n'                                                               -> tokenize(b, c+1)
     ' '                                                                -> tokenize(b, c)
     '\t'                                                               -> tokenize(b, c)
     '\r'                                                               -> tokenize(b, c)
     '/' -> do
       if b !! 0 == '/'
-        then ("//", c) : (tokenize (tail b, c))
-      else ("/", c) : (tokenize (tail b, c))
-    _                                                                  -> ([a], c) : (tokenize (b, c))
+        then ("//", c, Comment) : (tokenize (tail b, c))
+      else ("/", c, ForwardSlash) : (tokenize (tail b, c))
+    _                                                                  -> ([a], c, Other) : (tokenize (b, c))
 
 main = do
   filename <- getLine
